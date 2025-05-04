055231feeffec2273e389e6f74881ad2fb221b05
verkonia <v.azmukova@gmail.com>
2025-05-04 20:01:28 +0500

    add tests

---
+++ tests/main_test.py
@@ -0,0 +1,27 @@
+from fastapi.testclient import TestClient
+import faker
+from main import app
+
+client = TestClient(app)
+fake = faker.Faker()
+
+username = fake.user_name()
+password = fake.password()
+
+
+def test_register():
+    response = client.post("/register", params={"username": username, "password": password})
+    assert response.status_code == 200
+
+
+def test_login():
+    response = client.post("/register", params={"username": username, "password": password})
+    assert response.status_code == 200
+    assert "access_token" in response.json()
+    global token
+    token = response.json()["access_token"]
+
+def test_me():
+    response = client.get("/me", headers={"Authorization": f"Bearer {token}"})
+    assert response.status_code == 200
+    assert response.json()["username"] == username
